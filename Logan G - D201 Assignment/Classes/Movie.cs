78ce86835bafba57de9957df5d8def43b5db0c5c
C0ckR04ch0 <23370220@studentmail.ucol.ac.nz>
2026-05-10 16:36:34 +1200

    Seperated Movie into a seperate file called classes

---
+++ Logan G - D201 Assignment/Classes/Movie.cs
@@ -0,0 +1,23 @@
+﻿namespace Logan_G___D201_Assignment.Classes
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public bool Availablilty { get; set; }
+
+        public Movie(int movie_id, string title, string director, string genre, int release_year, bool availability)
+        {
+            MovieID = movie_id;
+            Title = title;
+            Director = director;
+            Genre = genre;
+            ReleaseYear = release_year;
+            Availablilty = availability;
+        }
+
+    }
+}
--- Logan G - D201 Assignment/MainWindow.xaml.cs
+++ Logan G - D201 Assignment/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Logan_G___D201_Assignment.Classes;
 
 namespace Logan_G___D201_Assignment
 {
@@ -15,27 +16,6 @@ namespace Logan_G___D201_Assignment
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-
-    public class Movie
-    {
-        public int MovieID { get; set; }
-        public string Title { get; set; }
-        public string Director { get; set; }
-        public string Genre { get; set; }
-        public int ReleaseYear { get; set; }
-        public bool Availablilty { get; set; }
-
-        public Movie(int movie_id, string title, string director, string genre, int release_year, bool availability)
-        {
-            MovieID = movie_id;
-            Title = title;
-            Director = director;
-            Genre = genre;
-            ReleaseYear = release_year;
-            Availablilty = availability;
-        }
-
-    }
 
     //public class MovieNode
     //{
