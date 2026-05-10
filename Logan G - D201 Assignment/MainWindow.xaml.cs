01bdcf2df43d155a10741299b58fb8467f3249a4
C0ckR04ch0 <23370220@studentmail.ucol.ac.nz>
2026-05-10 16:32:13 +1200

    Test Commit

--- Logan G - D201 Assignment/MainWindow.xaml.cs
+++ Logan G - D201 Assignment/MainWindow.xaml.cs
@@ -14,11 +14,58 @@ namespace Logan_G___D201_Assignment
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public bool Availablilty { get; set; }
+
+        public Movie(int movie_id, string title, string director, string genre, int release_year, bool availability)
+        {
+            MovieID = movie_id;
+            Title = title;
+            Director = director;
+            Genre = genre;
+            ReleaseYear = release_year;
+            Availablilty = availability;
+        }
+
+    }
+
+    //public class MovieNode
+    //{
+
+    //}
+
+    //public class MyLinkedList
+    //{
+
+    //}
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadMovieOnStartup();
+
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadMovieOnStartup()
+        {
+            Movie HTTYD = new Movie(1, "How to Train your Dragon", "Chris Sanders", "Fantasy", 2010, true);
+            dtgMovies.ItemsSource = new List<Movie>([HTTYD]);
         }
     }
 }
